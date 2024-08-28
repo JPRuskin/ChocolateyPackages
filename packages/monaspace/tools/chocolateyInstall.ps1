cbfde799d86fcd3dc005a26a2a1684b1ed8ce008
Cory Knox <30301021+corbob@users.noreply.github.com>
2024-08-28 08:12:18 -0700

    Fix monaspace upgrade failures
    If monaspace is already installed, it fails to upgrade because some of the font names can't be found with `Get-ItemProperty`. This changes the detection to a way that works for these names.

--- packages/monaspace/tools/chocolateyInstall.ps1
+++ packages/monaspace/tools/chocolateyInstall.ps1
@@ -5,13 +5,15 @@ $packageArgs = @{
     PackageName    = $env:ChocolateyPackageName
     FileFullPath   = Join-Path $toolsDir "monaspace.zip"
     Destination    = "$toolsDir/fonts"
-    Checksum       = '3E08376FD0AECA1F851FDE0C08E18CA2D797F6A4C7A449670BF4D1270303C8F6'
+    Checksum       = '7FF2317C7BDAED8E81DCBE1314E6AB12AD9641B7DDF921E996A227FF4EC7752F'
     ChecksumType   = 'sha256'
 }
 
 Get-ChocolateyUnzip @packageArgs
 
 $FontDirectory = (New-Object -ComObject Shell.Application).namespace(0x14).self.path
+$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts\'
+$RegisteredFonts = Get-ItemProperty -path $RegistryPath
 
 foreach ($Font in Get-ChildItem -Path "$toolsDir/fonts" -Include ('*.otf', '*.ttf') -Recurse) {
     Write-Verbose "Installing Font - $($Font.BaseName)"
@@ -20,11 +22,14 @@ foreach ($Font in Get-ChildItem -Path "$toolsDir/fonts" -Include ('*.otf', '*.tt
     # Register font for all users
     $FontRegistryEntry = @{
         Name         = $Font.BaseName
-        Path         = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+        Path         = $RegistryPath
         PropertyType = "String"
         Value        = $Font.Name
     }
-    if (-not (Get-ItemProperty -Path $FontRegistryEntry.Path -Name $FontRegistryEntry.Name -ErrorAction SilentlyContinue)) {
+    if (-not (Get-Member -InputObject $RegisteredFonts | ? Name -eq $FontRegistryEntry.Name)) {
+        Write-Host $FontRegistryEntry.Name
+        Get-Member -InputObject $RegisteredFonts | ? Name -eq $FontRegistryEntry.Name
+        Get-ItemProperty -Path $FontRegistryEntry.Path -Name $FontRegistryEntry.Name -ErrorAction SilentlyContinue
         $null = New-ItemProperty @FontRegistryEntry
     }
 }
